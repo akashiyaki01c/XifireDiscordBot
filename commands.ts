42d5c4867334b71472aed1d0874d0f56508df15b
Xifire <95431075+akashiyaki01c@users.noreply.github.com>
2023-09-24 00:08:53 +0900

    スラッシュコマンドに移行

--- commands.ts
+++ commands.ts
@@ -1,9 +1,17 @@
 import { discordeno } from "./deps.ts";
 
 export const commands = {
-    "neko": async (_bot: discordeno.Bot, _message: discordeno.Message) => {
-        _bot.helpers.sendMessage(_message.channelId, {
-            content: "にゃーん",
-        })
-    },
+    "neko": {
+        name: "neko",
+        description: "にゃーんと返します",
+        func: (_bot: discordeno.Bot, _interaction: discordeno.Interaction) => {
+            _bot.helpers.sendInteractionResponse(_interaction.id, _interaction.token, {
+                type: discordeno.InteractionResponseTypes.ChannelMessageWithSource,
+                data: {
+                    content: "にゃーん！！",
+                },
+            })
+
+        },
+    }
 };
--- main.ts
+++ main.ts
@@ -15,11 +15,26 @@ const bot = discordeno.createBot({
     },
 })
 
+await bot.helpers.createGuildApplicationCommand(commands.neko as discordeno.CreateSlashApplicationCommand, Secret.GUILD_ID)
+await bot.helpers.upsertGuildApplicationCommands(Secret.GUILD_ID, [commands.neko as discordeno.CreateSlashApplicationCommand])
+
 bot.events.messageCreate = (b, message) => {
     for (const command in commands) {
         if (message.content.startsWith(CommandStartChar+command)) {
             (async () => {
-                await ((commands as any)[command] as any)(b, message);
+                await ((commands as any)[command] as any).func(b, message);
+            })();
+        }
+    }
+}
+
+bot.events.interactionCreate = (b, interaction) => {
+    // interaction.data?.name
+    for (const command in commands) {
+        console.log(command, interaction.data?.name)
+        if ((commands as any)[command].name === interaction.data?.name) {
+            (async () => {
+                await ((commands as any)[command]).func(b, interaction);
             })();
         }
     }
